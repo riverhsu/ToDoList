9b6b3b0b930b9b40303fd2c3475f7056c0edb616
Sgmedical <sgmedical@sgs-macbook-air.local>
2016-10-19 17:37:58 +0800

    step 5. Ask AddTaskViewController to handle the new task. also, modify the missing part of entity. It is important to use "codegen" to generate entity class

--- ToDoList/AddTaskViewController.swift
+++ ToDoList/AddTaskViewController.swift
@@ -10,9 +10,30 @@ import Foundation
 import UIKit
 
 class AddTaskViewController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var isImp: UISwitch!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    @IBAction func btnTapped(_ sender: AnyObject) {
+        
+        // access persistentContainer.viewContext
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        let task = Task(context: context)
+        task.name = textField.text!
+        task.isImportant = isImp.isOn
+        
+        //save the data to coredata
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        // push the new task back to the navigation controller
+        navigationController!.popViewController(animated: true)
+        
+    }
     
 }
