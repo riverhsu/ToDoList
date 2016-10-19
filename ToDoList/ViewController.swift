387f173bc79441a4b56e801fc25c77d643e3bd56
Sgmedical <sgmedical@sgs-macbook-air.local>
2016-10-19 18:02:19 +0800

    step 5 : save data into core data and  fetch them immediately

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -8,12 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var tasks: [Task] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
     }
+    
+    //display tasks while loading them from core data
+    override func viewWillAppear(_ animated: Bool) {
+        // bring the data and load them from core data
+        getData()
+        
+        //reload the table view
+        tableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let task = tasks[indexPath.row]
+        
+        if task.isImportant{
+            cell.textLabel?.text = "😀 \(task.name!)"
+        } else {
+            cell.textLabel?.text = task.name!
+        }
+        
+        return cell
+    }
+    
+    // get data from core data entity
+    func getData(){
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        do {
+            tasks = try context.fetch(Task.fetchRequest())
+        } catch {
+            print("Fetching Failed")
+        }
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
