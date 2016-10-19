df1e6e5b2c8406994cf3eb2f53ac882db2b932f2
Sgmedical <sgmedical@sgs-macbook-air.local>
2016-10-19 15:45:06 +0800

    step 3. Embed the view in navigation control, and add a display view controller, AddTaskViewController. Also we appended a new text field for user's input

---
+++ ToDoList/AddTaskViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AddTaskViewController.swift
+//  ToDoList
+//
+//  Created by Sgmedical on 2016/10/19.
+//  Copyright © 2016年 Sgmedical. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddTaskViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
